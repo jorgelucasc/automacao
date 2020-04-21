149e22a0b7b444a5493220b0254d95f3016d7598
jorgelucasc <57646977+jorgelucasc@users.noreply.github.com>
2020-04-21 15:16:41 -0300

    Add files via upload

---
+++ CapyZn/spec/hello_spec.rb
@@ -0,0 +1,9 @@
+#projeto capybara do zero
+describe 'meu primeiro script' do
+    it 'Visitar a pagina' do
+        visit 'http://training-wheels-protocol.herokuapp.com/'
+        puts page.title
+        expect(page.title).to eql "Training Wheels Protocol"
+        sleep 5
+    end
+end
---
+++ RubyFt/README.md
@@ -0,0 +1,3 @@
+# projeto
+Iniciando automação
+Aulas de automação com Ruby, aprendente Programação em Ruby, BDD, com capybara.
