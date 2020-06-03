48bc3169c9f7c5c61e40e8837884ae55a296a440
jorgelucasc <jorge.lucas.18@hotmail.com>
2020-06-02 21:01:27 -0300

    primeiro step definition em cucumber

---
+++ nflix/features/step_definitions/login_step.rb
@@ -0,0 +1,31 @@
+Quando("faço lgin com {string} e {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("devo ser autenticado") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("devo ver {string} na area logada") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando("eu faço login com {string} e {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Entao("nao devo ser autenticado") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Entao("devo ver a mensagem de alerta {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando("eu faco login com {string} e {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Entao("devo ver a mensagme de alerta {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
